0f6531ebd4a5ab098f9d5fc7f80ab492925486d1
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-16 16:35:34 -0600

    completed milestone 1 / added css file

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Coding Chronicle</title>
+		<link rel="stylesheet" href="./style.css"/>
+	</head>
+	<body>
+		<h1 id="pageTitle">Coding Chronicle</h1>
+		<h2>Marlon McPherson's Journey Through the World of Web Development</h2>
+		<hr/>
+		<h2><span class="milestone">Milestone 1</span></h2>
+		<hr/>
+		<h3><span class="subject">Purpose, Audience, and Goals</span></h3>
+		<div class="container">
+			<p><strong>Purpose:</strong> This page will be a space in which I can document my progress as a coder during (and after) this bootcamp. I'll include links to github/social media/personal website and other things like reflective 'blog' posts, insights, and inspirations that I pick up along the way.</p>
+			<p><strong>Audience:</strong> The intended audience of this page consists of anyone who might be interested in my journey as a coder. Ideally, a potential employer or client will see this page and immediately understand my programming chops.</p>
+			<p><strong>Goals:</strong> My goal with this project is to create a space with which I can 'sell my skills'. The page will function as a living gallery of my progress in web development, and will showcase my skills for potential employers or clients.</p>
+		</div>
+		<h3><span class="subject">Persona</span></h3>
+		<div class="container">
+			<p><strong>Name:</strong> <a href="https://www.youtube.com/watch?v=HsSSfE87G_Y" title="The best fake name">Chim Richalds</a></p>
+			<p><strong>Age:</strong> 34</p>
+			<p><strong>Profession:</strong> Chim is the hiring manager for a web development firm. </p>
+			<p><strong>Technology:</strong> 2016 Macbook Pro, latest macOS version</p>
+			<p><strong>Attitude:</strong> Chim is a busy man, especially when a position opens up. He routinely views hundreds of resumes, so anything that can expedite the candidate selection process is welcome.</p>
+			<p><strong>Frustrations:</strong> Looking at a person's github page is a great way to determine what sort of coder they are, but not much else about that person is revealed just by scouring their repos. In a world where coding is increasingly becoming a team effort, Chim needs a way to select prime applicants with characteristics that may not be evident in a git repo. As a hiring representative, he wants more information about candidates before he schedules the all-important interview.</p>
+			<p><strong>Goals:</strong> Chim wants to be able to see more about an applicant than just their resume and github page. He wants to know about a person's creativity, adaptability, and personality - preferably in one place.</p>
+			<p><strong>User Story:</strong> As a hiring manager, Chim wants to know more about applicants before he schedules interviews.</p>
+		</div>
+	</body>
+</html>
