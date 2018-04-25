6f2f7e371e272eb69173e1af3ee1b0c9f3bffc54
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-25 14:54:26 -0600

    added milestone 2 file + prelim structure

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>title</title>
+	</head>
+	<body>
+		<h1 id="pageTitle">Coding Chronicle</h1>
+		<h2>Marlon McPherson's Journey Through the World of Web Development</h2>
+		<hr/>
+		<h2><span class="milestone">Milestone 2</span></h2>
+		<hr/>
+		<h3><span class="subject">Content Strategy</span></h3>
+		<div class="container">
+			<p><strong>Purpose:</strong> This page will be a space in which I can document my progress as a coder during (and after) this bootcamp. I'll include links to github/social media/personal website and other things like reflective 'blog' posts, insights, and inspirations that I pick up along the way.</p>
+			<p><strong>Audience:</strong> The intended audience of this page consists of anyone who might be interested in my journey as a coder. Ideally, a potential employer or client will see this page and immediately understand my programming chops.</p>
+			<p><strong>Goals:</strong> My goal with this project is to create a space with which I can 'sell my skills'. The page will function as a living gallery of my progress in web development, and will showcase my skills for potential employers or clients.</p>
+		</div>
+	</body>
+</html>
