525e1d61c078d7a9f74755792d1493a586b20a45
Marlon McPherson <mmcpherson5@cnm.edu>
2018-06-05 13:14:32 -0600

    added validated contact form, applied custom style.

--- public_html/index.php
+++ public_html/index.php
@@ -10,8 +10,25 @@
 		<link href="https://fonts.googleapis.com/css?family=Anonymous+Pro" rel="stylesheet">
 		<!--FontAwesome-->
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-		<!--Custom CSS-->
-		<link rel="stylesheet" href="styles/style.css">
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!--form-validate-->
+		<script src="js/form-validate.js"></script>
+
 		<title>Coding Chronicle</title>
 	</head>
 	<body>
@@ -19,25 +36,29 @@
 		<nav class="navbar navbar-expand-lg navbar-default fixed-top">
 			<a id="homeLink" class="navbar-brand" href="#top">coding-chronicle:~$ </a><span id="titleText" class="d-none d-xl-inline-block navbar-brand">Marlon <span id="mAlign">M</span><span id="supC">c</span>Pherson's course through the web dev world<span id="blockChar">&#9616;</span></span>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+				<span class="navbar-toggler-icon"><i class="navIcon fas fa-bars"></i></span>
 			</button>
 <!--			NAV LINKS-->
 
+<!--			todo URL redirect when ready to deploy!-->
+<!--			todo change resume email to marlon@codingchronicle.com-->
+<!--			todo create email acct on cpanel if I want to send mail from cc.com - use mozilla thunderbird-->
+<!--			todo need to make DB to use wordpress/drupal-->
 
 <!--			MAKE LINKS GO TO NEW TAB!-->
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item">
-						<a class="nav-link" href="https://github.com/mmcph"><i class="fab fa-github-square"></i></a>
+						<a class="nav-link" href="https://github.com/mmcph"><i class="navIcon fab fa-github-square"></i></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="https://www.linkedin.com/in/mmcph"><i class="fab fa-linkedin"></i></a>
+						<a class="nav-link" href="https://www.linkedin.com/in/mmcph"><i class="navIcon fab fa-linkedin"></i></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="https://twitter.com/mmcphdev"><i class="fab fa-twitter-square"></i></a>
+						<a class="nav-link" href="https://twitter.com/mmcphdev"><i class="navIcon fab fa-twitter-square"></i></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#"><i class="fas fa-envelope-square"></i></a>
+						<a class="nav-link" href="#bottom"><i class="navIcon fas fa-envelope-square"></i></a>
 					</li>
 			</div>
 		</nav>
@@ -98,12 +119,67 @@
 			</div>
 		</div>
 
+<div class="footer">
+	<div class="container">
+		<form id="pwp-form" method="post" action="php/mailer.php">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="form-group">
+						<label for="pwpName">Name</label>
+						<div class="input-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text"><i class="formIcon fa fa-user"></i></span>
+							</div>
+							<input class="form-control" type="text" name="pwpName" id="pwpName" placeholder="Your Name">
+						</div>
+					</div>
+				</div>
 
+				<div class="col-md-4">
+					<div class="form-group">
+						<label for="pwpEmail">Email</label>
+						<div class="input-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text"><i class="formIcon fa fa-envelope"></i></span>
+							</div>
+							<input class="form-control" type="email" name="pwpEmail" id="pwpEmail" placeholder="Your Email">
+						</div>
+					</div>
+				</div>
 
+				<div class="col-md-4">
+					<div class="form-group">
+						<label for="pwpSubject">Subject</label>
+						<div class="input-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text"><i class="formIcon fa fa-comment"></i></span>
+							</div>
+							<input class="form-control" type="text" name="pwpSubject" id="pwpSubject" placeholder="Subject">
+						</div>
+					</div>
+				</div>
+			</div>
 
-		<!--JQ/JS-->
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+			<div class="form-group">
+				<label for="pwpMessage">Message</label>
+				<div class="input-group">
+					<div class="input-group-prepend">
+						<span class="input-group-text"><i class="formIcon fas fa-edit"></i></span>
+					</div>
+					<textarea name="pwpMessage" rows="6" id="pwpMessage" class="form-control" placeholder="Your Message (2000 characters max)"></textarea>
+				</div>
+			</div>
+
+			<!--							RECAPTCHA-->
+			<div class="g-recaptcha" data-sitekey="6LenZlwUAAAAACHelIgj395loGCGGoFSmmm1IGUq"></div>
+
+			<button class="btn btn-info" type="submit">Submit</button>
+			<button class="btn btn-default" type="reset">Reset</button>
+
+		</form>
+	</div>
+</div>
+
+<div id="bottom"></div>
 	</body>
 </html>
